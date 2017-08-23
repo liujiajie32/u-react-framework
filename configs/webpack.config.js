653966670ab3275156ddb38b953984c9bd1fdad5
liujiajie32 <liujiajie32@gmail.com>
2017-08-23 17:35:19 +0800

    initialize project structure

---
+++ configs/webpack.config.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+// const CommonChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin"); 
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Folder path definition
+const ROOT_PATH = path.resolve(__dirname, '../');
+const NODE_MODULES_PATH = path.resolve(ROOT_PATH, 'node_modules');
+const JS_DIST_PATH = path.resolve(ROOT_PATH, 'dist/js');
+
+
+// Judge if there's an argument '-p' or '--production' in script
+var isProduction = false;
+for (let i in process.argv) {
+  if (process.argv[i] === '-p' || process.argv[i] === '--production') {
+    isProduction = true;
+    break;
+  }
+}
+var outputName  = isProduction ? 'u.all.min' : 'u.all';
+
+var cleanPath = [
+  path.resolve(ROOT_PATH, '*.zip'),
+  path.resolve(JS_DIST_PATH, '*.html'),
+];
+
+if(isProduction) {
+  cleanPath.push(path.resolve(JS_DIST_PATH, '*.min.js'));
+} else {
+  cleanPath.push(path.resolve(JS_DIST_PATH, '*.js'));
+}
+
+
+module.exports = {
+  entry:  {
+    [outputName]: [
+      './src/componentA/componentA'
+    ],
+    'componentA': './src/componentA/componentA',
+    'componentB': './src/componentB/componentB',
+  },
+  output: {
+    path: JS_DIST_PATH,
+    filename: '[name].js'
+  },
+  plugins: [
+    // Abstract common chunks to vendor.js
+    // new CommonChunkPlugin({name: 'vendors', filename:'vendors.js'}),
+    // Clean dist folder before compiling
+    new CleanWebpackPlugin(cleanPath, {
+      root: ROOT_PATH,
+      verbose: true // open console information output
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Test APP',
+      filename: '../html/index.html',
+      chunks: [outputName]
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Test ComponentA',
+      filename: '../html/test-componentA.html',
+      chunks: ['componentA']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Test ComponentB',
+      filename: '../html/test-componentB.html',
+      chunks: ['componentB']
+    })
+  ],
+  module: {
+    loaders:[{
+      // babel loader
+      test:/\.(js|jsx)$/,
+      loader: 'babel-loader',
+      exclude: /(node_modules)/,
+      // include: JS_DIST_PATH,
+      query: {
+        presets: ['es2015', 'stage-1', 'react'],
+        plugins: [
+          ["transform-runtime", {
+            "helpers": false,
+            "polyfill": true,
+            "regenerator": true
+          }],
+          "add-module-exports",
+          "transform-es3-member-expression-literals",
+          "transform-es3-property-literals"
+        ]
+      }
+    }]
+  },
+
+  resolve:{
+    extensions:['.js', '.jsx', '.css']
+  }
+
+
+
+}
